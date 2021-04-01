c60873c623a024c0cb08cb7061ae2ce376362022
Ramon Smits <ramon.smits@gmail.com>
2021-04-01 13:45:10 +0200

    Commandline options

--- FakeMessageGen/Program.cs
+++ FakeMessageGen/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using App.Metrics;
-using App.Metrics.Counter;
 using App.Metrics.Meter;
 using App.Metrics.Scheduling;
 using NServiceBus;
@@ -14,37 +10,48 @@ using NServiceBus.Extensibility;
 using NServiceBus.Logging;
 using NServiceBus.Raw;
 using NServiceBus.Routing;
-using NServiceBus.Sagas;
 using NServiceBus.Transport;
 using ServiceControl.Transports;
 using ServiceControl.Transports.ASBS;
 
 static partial class Program
 {
-    const string destination = "audit";
+    static string destination;
+    static bool isError;
 
-    const int MaxConcurrency = 100;
-    const int MaxQueueLength = 1000;
+    static int MaxConcurrency = 100;
+    static int MaxQueueLength = 1000;
+    static int RateLimit = 1000;
 
     static readonly object lockObj = new();
-    static readonly RateGate rateLimiter = new(200, TimeSpan.FromSeconds(1));
-    static readonly string prefix = Guid.NewGuid().ToString("n") + "/" + DateTime.UtcNow.ToString("s") + "/";
     static readonly SemaphoreSlim concurrency = new(MaxConcurrency);
-
+    static RateGate rateLimiter; 
     static IReceivingRawEndpoint sender;
     static State state = State.Running;
-    static int count;
 
-    static async Task Main()
+    static async Task Main(string[] args)
     {
+        try
+        {
+            destination = args[0];
+            isError = bool.Parse(args[1]);
+            if (args.Length > 2) MaxQueueLength = int.Parse(args[2]);
+            if (args.Length > 3) RateLimit = int.Parse(args[3]);
+            if (args.Length > 4) MaxConcurrency = int.Parse(args[4]);
+        }
+        catch
+        {
+            Console.WriteLine("destination isError (maxQueueLength) (rateLimit) (maxConcurrency)");
+            return;
+        }
+
+        rateLimiter = new(RateLimit, TimeSpan.FromSeconds(1));
+
         Console.OutputEncoding = Encoding.UTF8;
 
         InitFrames();
 
-        //LogManager.Use<DefaultFactory>().Level(LogLevel.Debug);
         LogManager.UseFactory(new FrameLoggerFactory(logFrame));
-
-        FrameMaster.Out.WriteLine("Pause, \u001b[31mHello World!\u001b[0m...");
 
         var metrics = new MetricsBuilder()
             .Report.ToConsole(o =>
@@ -53,8 +60,6 @@ static partial class Program
                 o.FlushInterval = TimeSpan.FromSeconds(5);
             })
             .Build();
-
-        var sent = new CounterOptions {Name = "sent"};
 
         var rate = new MeterOptions
         {
@@ -92,13 +97,11 @@ static partial class Program
 
                     if (result == State.Running)
                     {
-                        queueFrame.WriteLine("Pause, draining semaphore...");
+                        queueFrame.WriteLine("Pause");
                         for (var x = 0; x < 100; x++)
                         {
                             concurrency.Wait();
                         }
-
-                        queueFrame.WriteLine("Drained!");
                     }
                 }
                 else
@@ -108,7 +111,7 @@ static partial class Program
 
                     if (result == State.Paused)
                     {
-                        queueFrame.WriteLine("Continue, releasing");
+                        queueFrame.WriteLine("Continue");
                         concurrency.Release(100);
                     }
                 }
@@ -127,16 +130,14 @@ static partial class Program
             {
                 await rateLimiter.WaitAsync();
                 await concurrency.WaitAsync();
-                metrics.Measure.Counter.Increment(sent);
                 metrics.Measure.Meter.Mark(rate);
 
-                _ = Send(false);
+                _ = Send(isError);
             }
         });
 
         do
         {
-//            await mainFrame.WriteLineAsync("Press ESC to exit...");
             Console.WriteLine("Press ESC to exit...");
         } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
@@ -144,65 +145,13 @@ static partial class Program
         await queueLengthProvider.Stop();
     }
 
-    const string Sentences = "ABCD EFGH IJKL MNOP QRST UVWX YZ01 2345 6789\n";
-    const string Lines = "ABCD EFGH IJKL MNOP QRST UVWX YZ01 2345 6789";
-    const string Types = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.";
-
-    static string RandomString(int length, string chars)
-    {
-        return new string(Enumerable.Repeat(chars, length)
-            .Select(s => s[StaticRandom.Next(s.Length)]).ToArray());
-    }
-
     static async Task Send(bool isError)
     {
         try
         {
-            var body = new byte[StaticRandom.Next(1000, 10000)];
+            var msg = FakeMessageGenerator.Create(isError);
 
-            StaticRandom.NextBytes(body);
-
-
-            var now = DateTime.UtcNow;
-
-            var intents = new[] {"Send", "Publish", "Reply"};
-            var headers = new Dictionary<string, string>
-            {
-                [Headers.MessageId] = prefix + Interlocked.Increment(ref count),
-                [Headers.ContentType] = "random",
-                [Headers.EnclosedMessageTypes] = "random_" + StaticRandom.Next(250),
-                [Headers.CorrelationId] = now.ToString("yyyy-M-d hh") + " " + StaticRandom.Next(100000),
-                [Headers.ConversationId] = now.ToString("yyyy-M-d hh") + " " + StaticRandom.Next(100000),
-                //[Headers.RelatedTo] = "random",
-                [Headers.MessageIntent] = intents[StaticRandom.Next(3)],
-                [Headers.TimeSent] = DateTimeExtensions.ToWireFormattedString(now),
-                ["NServiceBus.OriginatingEndpoint"] = "endpoint_" + StaticRandom.Next(100),
-                ["NServiceBus.OriginatingMachine"] = Environment.MachineName
-            };
-
-            if (isError)
-            {
-                headers["NServiceBus.FailedQ"] = "endpoint_" + StaticRandom.Next(100);
-                headers["NServiceBus.ExceptionInfo.ExceptionType"] = RandomString(StaticRandom.Next(15, 250), Types);
-                headers["NServiceBus.ExceptionInfo.InnerExceptionType"] = RandomString(StaticRandom.Next(15, 250), Types);
-                headers["NServiceBus.ExceptionInfo.Message"] = RandomString(StaticRandom.Next(15, 250), Lines);
-                headers["NServiceBus.ExceptionInfo.StackTrace"] =
-                    RandomString(StaticRandom.Next(1000, 8000), Sentences);
-                //["NServiceBus.ExceptionInfo.HelpLink"] //The exception help link.
-                //["NServiceBus.ExceptionInfo.Source"] //The full type name of the InnerException if it exists. It is obtained by calling Exception.InnerException.GetType().FullName.
-            }
-            else
-            {
-                headers["NServiceBus.ProcessingEndpoint"] = "endpoint_" + StaticRandom.Next(100);
-                headers["NServiceBus.ProcessingMachine"] = Environment.MachineName;
-                headers["NServiceBus.ProcessingStarted"] = DateTimeExtensions.ToWireFormattedString(now);
-                headers["NServiceBus.ProcessingEnded"] = DateTimeExtensions.ToWireFormattedString(now.AddMilliseconds(StaticRandom.Next(20, 20000)));
-            }
-
-            var request = new OutgoingMessage(
-                messageId: Guid.NewGuid().ToString(),
-                headers: headers,
-                body: body);
+            var request = new OutgoingMessage(msg.id, headers: msg.headers, body: msg.body);
 
             var operation = new TransportOperation(
                 request,
@@ -218,4 +167,5 @@ static partial class Program
             concurrency.Release();
         }
     }
+
 }
